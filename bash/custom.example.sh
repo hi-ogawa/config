99a5b95821a4048436218dae5c1482c0da5a671d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-06-11 16:45:29 +0900

    feat(bash): change PS1 when loading depot tools (#4)

--- bash/custom.example.sh
+++ bash/custom.example.sh
@@ -3,6 +3,7 @@
 # cf. https://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html
 ho_custom_depot_tools() {
   export PATH="$HOME/code/installed/depot_tools:$PATH"
+  export PS1="(depot) $PS1"
 }
 
 ho_custom_default() {
