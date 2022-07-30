e5694ddf58c670377d3399a8773cf24c884f94b4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-30 18:59:06 +0900

    feat(bash): customize PROMPT_COMMAND

--- bash/export.sh
+++ bash/export.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-export PS1="$ "
 export EDITOR="nano"
 export PATH="$HOME/.local/bin:$PATH"
+
+# https://man.archlinux.org/man/bash.1.en#PROMPTING
+ho_prompt_command() {
+  local status="$?"
+  local status_message=""
+  if [ "$status" != "0" ]; then
+    status_message="($status) "
+  fi
+  local color_cwd="\[\033[0;32m\]"    # green
+  local color_status="\[\033[0;31m\]" # red
+  local color_restore="\[\033[0m\]"
+  export PS1="$color_cwd\W $color_status$status_message$color_restore$ "
+}
+export PROMPT_COMMAND=ho_prompt_command
