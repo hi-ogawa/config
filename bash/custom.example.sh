bde97b8b81e88ff199f3a4d3c29390673897439e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-01 14:04:17 +0900

    chore(bash): cleanup custom.example.sh

--- bash/custom.example.sh
+++ bash/custom.example.sh
@@ -6,29 +6,13 @@ ho_custom_depot_tools() {
   export PS1="(depot) $PS1"
 }
 
-# force reset driver when touchpad stucks on dell inspiron laptop
-ho_custom_rescue_hid() {
-  sudo modprobe -r hid_multitouch
-  sudo modprobe hid_multitouch
-}
-
-ho_custom_gpg_tty() {
-  GPG_TTY=$(tty)
-  export GPG_TTY
-}
-
-# https://wiki.archlinux.org/title/python#Python_2
-ho_python2() {
-  python2_bin="$XDG_RUNTIME_DIR/python2_bin"
-  mkdir -p "$python2_bin"
-  ln -sf /usr/bin/python2 "$python2_bin/python"
-  ln -sf /usr/bin/python2-config "$python2_bin/python-config"
-  export PATH="$python2_bin:$PATH"
+ho_android() {
+  export ANDROID_HOME="$HOME/Android/Sdk"
 }
 
 ho_custom_default() {
   ho_setup_volta
-  ho_custom_gpg_tty
+  ho_android
   # ho_setup_pyenv
   # ho_npm_completion
 }
